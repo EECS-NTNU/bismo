975f94a9515a5d053d62cd48c091cdbf8c6d2eeb
Yaman Umuroglu <yamanu@xilinx.com>
2018-07-12 20:20:50 +0100

    more work on SW-side instruction generation

--- src/main/cpp/app/BISMOInstruction.hpp
+++ src/main/cpp/app/BISMOInstruction.hpp
@@ -15,16 +15,13 @@ typedef struct {
 } BISMOInstruction;
 
 typedef struct {
-  uint64_t targetStage   : BISMO_INSTR_TARGETS_BITS;
+  uint64_t unused0      : 64;
+  uint64_t unused1      : 61;
   uint64_t isRunCfg      : 1;
-  uint64_t unused0      : 61;
-  uint64_t unused1      : 64;
+  uint64_t targetStage   : BISMO_INSTR_TARGETS_BITS;
 } BISMOSyncInstruction;
 
 typedef struct {
-  uint64_t targetStage   : BISMO_INSTR_TARGETS_BITS;
-  uint64_t isRunCfg      : 1;
-  uint64_t unused0       : 3;
   // DRAM fetch config
   // base address for all fetch groups
   uint64_t dram_base    : BISMO_INSTR_DRAMADR_BITS;
@@ -45,4 +42,43 @@ typedef struct {
   // ID range of BRAM to end at. start+range will be included.
   uint64_t bram_range   : BISMO_INSTR_FETCHID_BITS;
 
+  uint64_t unused0       : 3;
+  uint64_t isRunCfg      : 1;
+  uint64_t targetStage   : BISMO_INSTR_TARGETS_BITS;
 } BISMOFetchRunInstruction;
+
+typedef struct {
+  uint64_t lhsOffset : BISMO_INSTR_INBFADR_BITS;
+  uint64_t rhsOffset : BISMO_INSTR_INBFADR_BITS;
+  uint64_t numTiles : BISMO_INSTR_INBFADR_BITS;
+  // how much left shift to use
+  uint64_t shiftAmount : BISMO_INSTR_MAXSHFT_BITS;
+  // negate during accumulation
+  uint64_t negate : 1;
+  // clear accumulators prior to first accumulation
+  uint64_t clear_before_first_accumulation : 1;
+  // write to result memory at the end of current execution
+  uint64_t writeEn : 1;
+  // result memory address to use for writing
+  uint64_t writeAddr : BISMO_INSTR_RESADDR_BITS;
+  uint64_t unused0       : 7;
+  uint64_t unused1       : 61;
+  uint64_t isRunCfg      : 1;
+  uint64_t targetStage   : BISMO_INSTR_TARGETS_BITS;
+} BISMOExecRunInstruction;
+
+
+typedef struct {
+  // DRAM controls
+  uint64_t dram_base : BISMO_INSTR_DRAMADR_BITS;
+  uint64_t dram_skip : BISMO_INSTR_BLCKSIZ_BITS;
+  // wait for completion of all writes (no new DRAM wr generated)
+  uint64_t waitCompleteBytes : BISMO_INSTR_BLCKSIZ_BITS;
+  uint64_t waitComplete : 1;
+  // result memory to read from
+  uint64_t resmem_addr : BISMO_INSTR_RESADDR_BITS;
+  uint64_t unused0       : 62;
+  uint64_t isRunCfg      : 1;
+  uint64_t targetStage   : BISMO_INSTR_TARGETS_BITS;
+
+} BISMOResultRunInstruction;
