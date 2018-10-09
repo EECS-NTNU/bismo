611e5d8266ab71443d4fdc0d56c6651893fc70cd
Yaman Umuroglu <maltanar@gmail.com>
2018-10-10 00:58:53 +0100

    [ExecInstrGen] copy-paste unoptimized instrgen into HLS

--- src/main/hls/ExecInstrGen.cpp
+++ src/main/hls/ExecInstrGen.cpp
@@ -38,7 +38,105 @@
 #include <stdint.h>
 #include "BISMOInstruction.hpp"
 
+typedef struct {
+  // number of tiles in a single binary matrix
+  // expressed in terms of the instantiated DPA size
+  uint16_t tiles_m;
+  uint16_t tiles_k;
+  uint16_t tiles_n;
+  // number of bits in input matrices
+  uint8_t bits_l;
+  uint8_t bits_r;
+  // signedness for the input matrices
+  bool signed_l;
+  bool signed_r;
+  // base addresses for buffer accesses
+  uint16_t base_l;
+  uint16_t base_r;
+  uint8_t base_res;
+  // number of buffers for latency hiding
+  uint8_t nbufs_res;
+} SingleMMDescriptor;
+
 void ExecInstrGen(
+  hls::stream<SingleMMDescriptor> & desc_in,
+  hls::stream<ap_uint<128>> & out
+) {
+  #pragma HLS INTERFACE ap_ctrl_none port=return
+  #pragma HLS INTERFACE axis port=out
+  #pragma HLS INTERFACE axis port=desc_in
+  #pragma HLS DATA_PACK variable=desc_in
+
+  BISMOExecRunInstruction exec;
+  BISMOSyncInstruction sync;
+  #pragma HLS DATA_PACK variable=exec
+  #pragma HLS DATA_PACK variable=sync
+
+  SingleMMDescriptor in = desc_in.read();
+  // TODO: convert the software ExecInstrGenSingleMM to an HLS-friendly
+  // format and call directly instead of copy-paste
+  // all instructions are targeting the execute stage
+  sync.targetStage = stgExec;
+  // start by acquiring input buffers
+  sync.isRunCfg = 0;
+  sync.isSendToken = 0;
+  sync.chanID = 0;
+  out.write(sync.asRaw());
+  // keep track of which result buffer we wrote to last
+  size_t offset_res = 0;
+  for(size_t m = 0; m < in.tiles_m; m++) {
+    for(size_t n = 0; n < in.tiles_n; n++) {
+      // starting a new result tile:
+      // acquire a result buffer
+      sync.isRunCfg = 0;
+      sync.isSendToken = 0;
+      sync.chanID = 1;
+      out.write(sync.asRaw());
+      for(size_t l = 0; l < in.bits_l; l++) {
+        for(size_t r = 0; r < in.bits_r; r++) {
+          // helper variables based on current loop iteration
+          bool tile_first = (l == 0) && (r == 0);
+          bool tile_last = (l == in.bits_l-1) && (r == in.bits_r-1);
+          size_t weight = l + r;
+          // whether the current bit position is negative for
+          // the input matrices
+          bool neg_l = (l == in.bits_l-1) && in.signed_l;
+          bool neg_r = (r == in.bits_r-1) && in.signed_r;
+          bool negate = neg_l ^ neg_r;
+          size_t offset_l = in.tiles_k * (m + l * in.tiles_m);
+          size_t offset_r = in.tiles_k * (n + r * in.tiles_n);
+          // switch result buffers for latency hiding
+          offset_res = (offset_res + 1) % in.nbufs_res;
+          exec.isRunCfg = 1;
+          exec.lhsOffset = in.base_l + offset_l;
+          exec.rhsOffset = in.base_r + offset_r;
+          exec.numTiles = in.tiles_k;
+          exec.shiftAmount = weight;
+          exec.negate = negate ? 1 : 0;
+          // clear accumulator on first iteration of this result tile
+          exec.clear_before_first_accumulation = tile_first ? 1 : 0;
+          // write result on first iteration of this result tile
+          exec.writeEn = tile_last ? 1 : 0;
+          exec.writeAddr = in.base_res + offset_res;
+          out.write(exec.asRaw());
+        }
+      }
+      // finished computing result tile
+      // release the result buffer
+      sync.isRunCfg = 0;
+      sync.isSendToken = 1;
+      sync.chanID = 1;
+      out.write(sync.asRaw());
+    }
+  }
+  // release the input buffers
+  sync.isRunCfg = 0;
+  sync.isSendToken = 1;
+  sync.chanID = 0;
+  out.write(sync.asRaw());
+}
+
+void VerifyExecInstrEncoding(
   hls::stream<ap_uint<128>> & out
 ) {
   #pragma HLS INTERFACE ap_ctrl_none port=return
