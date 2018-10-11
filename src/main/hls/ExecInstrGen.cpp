bcb7fc7ba2c4722cd25825b8d6193451eb4789c1
Yaman Umuroglu <yamanu@xilinx.com>
2018-10-11 13:45:38 +0100

    [ExecInstrGen] HLS optimizations for the ExecInstrGen from HLS_SYN_DSP=3,HLS_SYN_FF=10202,HLS_SYN_LUT=7684 to HLS_SYN_DSP=9,HLS_SYN_FF=1903,HLS_SYN_LUT=1137

--- src/main/hls/ExecInstrGen.cpp
+++ src/main/hls/ExecInstrGen.cpp
@@ -51,9 +51,6 @@ void ExecInstrGen(
 
   SingleMMDescriptor ins_in;
   ins_in.fromRaw(in.read());
-  // TODO: convert the software ExecInstrGenSingleMM to an HLS-friendly
-  // format and call directly instead of copy-paste
-  // invariants
   sync.targetStage = stgExec;
   exec.targetStage = stgExec;
   exec.isRunCfg = 1;
@@ -62,49 +59,77 @@ void ExecInstrGen(
   sync.isSendToken = 0;
   sync.chanID = 0;
   out.write(sync.asRaw());
-  // keep track of which result buffer we wrote to last
-  size_t offset_res = 0;
-  for(size_t m = 0; m < ins_in.tiles_m; m++) {
-    for(size_t n = 0; n < ins_in.tiles_n; n++) {
+  // compute the size of the iteration space
+  const size_t total_iters = ins_in.tiles_m * ins_in.tiles_n * ins_in.bits_l * ins_in.bits_r;
+  /// iteration variables
+  uint16_t m = 0, n = 0;
+  uint8_t l = 0, r = 0;
+  uint8_t offset_res = 0;
+  // single iteration space for the entire instrgen
+  for(size_t i = 0; i < total_iters; i++) {
+    // helper variables based on current loop iteration
+    const bool tile_first = (l == 0) && (r == 0);
+    const bool tile_last = (l == ins_in.bits_l-1) && (r == ins_in.bits_r-1);
+    if(tile_first) {
       // starting a new result tile:
       // acquire a result buffer
       sync.isRunCfg = 0;
       sync.isSendToken = 0;
       sync.chanID = 1;
       out.write(sync.asRaw());
-      for(size_t l = 0; l < ins_in.bits_l; l++) {
-        for(size_t r = 0; r < ins_in.bits_r; r++) {
-          // helper variables based on current loop iteration
-          bool tile_first = (l == 0) && (r == 0);
-          bool tile_last = (l == ins_in.bits_l-1) && (r == ins_in.bits_r-1);
-          size_t weight = l + r;
-          // whether the current bit position is negative for
-          // the input matrices
-          bool neg_l = (l == ins_in.bits_l-1) && ins_in.signed_l;
-          bool neg_r = (r == ins_in.bits_r-1) && ins_in.signed_r;
-          bool negate = neg_l ^ neg_r;
-          size_t offset_l = ins_in.tiles_k * (m + l * ins_in.tiles_m);
-          size_t offset_r = ins_in.tiles_k * (n + r * ins_in.tiles_n);
-          // switch result buffers for latency hiding
-          offset_res = (offset_res + 1) % ins_in.nbufs_res;
-          exec.lhsOffset = ins_in.base_l + offset_l;
-          exec.rhsOffset = ins_in.base_r + offset_r;
-          exec.numTiles = ins_in.tiles_k;
-          exec.shiftAmount = weight;
-          exec.negate = negate ? 1 : 0;
-          // clear accumulator on first iteration of this result tile
-          exec.clear_before_first_accumulation = tile_first ? 1 : 0;
-          // write result on first iteration of this result tile
-          exec.writeEn = tile_last ? 1 : 0;
-          exec.writeAddr = ins_in.base_res + offset_res;
-          out.write(exec.asRaw());
-        }
-      }
+    }
+    const uint8_t weight = l + r;
+    // whether the current bit position is negative for
+    // the input matrices
+    const bool lbit_last = (l == ins_in.bits_l-1);
+    const bool rbit_last = (r == ins_in.bits_r-1);
+    const bool neg_l = lbit_last && ins_in.signed_l;
+    const bool neg_r = rbit_last && ins_in.signed_r;
+    bool negate = neg_l ^ neg_r;
+    // TODO consider removing mults here, use mod counters
+    // offset_l = ins_in.tiles_k * m + ins_in.tiles_k * ins_in.tiles_m * l;
+    // offset_r = ins_in.tiles_k * n + ins_in.tiles_k * ins_in.tiles_n * r;
+    const uint16_t offset_l = ins_in.tiles_k * (m + l * ins_in.tiles_m);
+    const uint16_t offset_r = ins_in.tiles_k * (n + r * ins_in.tiles_n);
+    exec.lhsOffset = ins_in.base_l + offset_l;
+    exec.rhsOffset = ins_in.base_r + offset_r;
+    exec.numTiles = ins_in.tiles_k;
+    exec.shiftAmount = weight;
+    exec.negate = negate ? 1 : 0;
+    // clear accumulator on first iteration of this result tile
+    exec.clear_before_first_accumulation = tile_first ? 1 : 0;
+    // write result on first iteration of this result tile
+    exec.writeEn = tile_last ? 1 : 0;
+    exec.writeAddr = ins_in.base_res + offset_res;
+    out.write(exec.asRaw());
+    if(tile_last) {
       // finished computing result tile
       // release the result buffer
       sync.isSendToken = 1;
       sync.chanID = 1;
       out.write(sync.asRaw());
+    }
+    // iteration tracking logic: result buffer offset
+    offset_res++;
+    if(offset_res == ins_in.nbufs_res) {
+      offset_res = 0;
+    }
+    // iteration tracking logic: nested loops over tiles and bits
+    r++;
+    if(r == ins_in.bits_r) {
+      r = 0;
+      l++;
+      if(l == ins_in.bits_l) {
+        l = 0;
+        n++;
+        if(n == ins_in.tiles_n) {
+          n = 0;
+          m++;
+          if(m == ins_in.tiles_m) {
+            m = 0;
+          }
+        }
+      }
     }
   }
   // release the input buffers
