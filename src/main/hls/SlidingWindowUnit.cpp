fbcab71d8bc3a742645567ef2c071ef91a46e9cd
DavideConficconi <davideconficconi@gmail.com>
2019-01-11 12:17:35 +0000

    [SWU] Update interface with input and output stream
    Signed-off-by: DavideConficconi <davideconficconi@gmail.com>

--- src/main/hls/SlidingWindowUnit.cpp
+++ src/main/hls/SlidingWindowUnit.cpp
@@ -1,6 +1,7 @@
 #include <ap_int.h>
 #include <stdint.h>
 #include <hls_stream.h>
+//#define DEBUG 0
 
 using namespace hls;
 //Sliding window unit: Assuming to work with single channel
@@ -8,28 +9,33 @@ using namespace hls;
 // Assuming Square dimensions for image, kernel and stride.
 // Outputs the indices of the input matrix to consider for a matrix matrix multiplication
 void SlidingWindowUnit(
-		//This should a single data structure or a stream combined
-//		hls::stream<ap_uint<128>> &strm_in,
-//		hls::stream<ap_uint<32>> &strm_out,
-		ap_uint<32> * out_mem_address,
-		bool & out_err,
-		ap_uint<32> in_base_address,
-		ap_uint<32> in_img_size,
-		ap_uint<32> in_krnl_size,
-		ap_uint<32> in_stride)
+		//128 bits: 32 for BASE Address, 32 for Image size, 32 Kernel size, 32 Stride
+		hls::stream<ap_uint<128>> &strm_in,
+		hls::stream<ap_uint<32>> &strm_out,
+		bool &out_err)
 {
-//#pragma HLS INTERFACE ap_ctrl_none port=return
-//#pragma HLS interface axis port=strm_out
-//#pragma HLS interface axis port=strm_in
+#pragma HLS INTERFACE ap_ctrl_none port=return
+#pragma HLS interface axis port=strm_out
+#pragma HLS interface axis port=strm_in
+
+	ap_uint<128> in_tmp = strm_in.read();
+	ap_uint<32> base_addr = in_tmp;
+	ap_uint<32> img_size = in_tmp >> 32;
+	ap_uint<32> krnl_size = in_tmp >> 64;
+	ap_uint<32> stride = in_tmp >> 96;
+//	if(DEBUG){
+//		std::cout << in_tmp << std::endl;
+//		printf("First 32 bits %x\n", static_cast<int>(base_addr) );
+//		printf("Second 32 bits %x\n", static_cast<int>(img_size)  );
+//		printf("Third 32 bits%x\n", static_cast<int>(krnl_size)  );
+//		printf("Fourth 32 bits%x\n", static_cast<int>(stride)  );
+//	}
 	//Run time check for computability
 	//TODO Needed? or assuming that this control is performed by a "compiler"
 //	out_err = !( (in_img_size - in_krnl_size) %  in_stride == 0);
-	ap_uint<32> img_size = in_img_size;
-	ap_uint<32> base_addr = in_base_address;
-	ap_uint<32> krnl_size = in_krnl_size;
-	ap_uint<32> stride = in_stride;
+
 	//Formula from zero padding arbitrary stride convolution
-	ap_uint<32> mat_out_dim = ((in_img_size - in_krnl_size)/ in_stride + 1);
+	ap_uint<32> mat_out_dim = ((img_size - krnl_size)/ stride + 1);
 	//Number of iterations of this convolution
 	ap_uint<32> iter_number = mat_out_dim * mat_out_dim;
 	//Input matrix iteration
@@ -37,20 +43,19 @@ void SlidingWindowUnit(
 
 #pragma HLS PIPELINE II=1
 //#pragma HLS LOOP_FLATTEN
-IMG_COL_LOOP: for(int j=0; j < img_size; j= j + stride){
+		IMG_COL_LOOP: for(int j=0; j < img_size; j= j + stride){
 //#pragma HLS PIPELINE II=1
 			//Kernel iteration
 //			std::cout << std::endl;
 //			std::cout << i << ", " << j << std::endl;
-			bool kernel_inbound = i-1 + in_krnl_size < in_img_size && j-1 + in_krnl_size < in_img_size;
+			bool kernel_inbound = i-1 + krnl_size < img_size && j-1 + krnl_size < img_size;
 			//Inbound check not needed in the inside loop
-			KRNL_ROW_LOOP: for(int r = i; r < in_krnl_size + i && kernel_inbound; r++)
-				KRNL_COL_LOOP: for(int c = j; c < in_krnl_size + j; c++){
+			KRNL_ROW_LOOP: for(int r = i; r < krnl_size + i && kernel_inbound; r++)
+				KRNL_COL_LOOP: for(int c = j; c < krnl_size + j; c++){
 //#pragma HLS PIPELINE II=1
-					ap_uint<32> outElem = r * in_img_size + c;
+					ap_uint<32> outElem = r * img_size + c;
 //					std::cout << outElem << " ";// << std::endl;
-//					strm_out.write(outElem);//.write(outElem);
-					*out_mem_address = outElem;
+					strm_out.write(outElem);
 				}
 			}
 
--- src/main/hls/SlidingWindowUnitTestBench.cpp
+++ src/main/hls/SlidingWindowUnitTestBench.cpp
@@ -2,18 +2,14 @@
 #include <stdint.h>
 #include <hls_stream.h>
 #include <stdio.h>
-#define IMG_SIZE 5
-#define KRNL_SIZE 3
+#define BASE_ADDRESS 0
+#define IMG_SIZE 4
+#define KRNL_SIZE 2
 #define STRIDE 1
 void SlidingWindowUnit(
-		ap_uint<32> in_base_addres,
-		ap_uint<32> in_img_size,
-		ap_uint<32> in_krnl_size,
-		ap_uint<32> in_stride,
-		bool &out_err,
-//		ap_uint<32> & out_mem_address
-		hls::stream<ap_uint<32>> & out_mem_address
-);
+		hls::stream<ap_uint<128>> &strm_in,
+		hls::stream<ap_uint<32>> &strm_out,
+		bool & out_err);
 int main(int argc, char * argv[]){
 	int test_mat [IMG_SIZE][IMG_SIZE];
 	for(int i = 0; i < IMG_SIZE; i++)
@@ -23,9 +19,27 @@ int main(int argc, char * argv[]){
 	int mat_out_dim = ((IMG_SIZE - KRNL_SIZE)/ STRIDE + 1);
 //	std::cout << mat_out_dim << std::endl;
 	hls::stream<ap_uint<32>> out_mem_address;
-
-	SlidingWindowUnit(0,IMG_SIZE,KRNL_SIZE,STRIDE,error,out_mem_address);
-
+	hls::stream<ap_uint<128>> in_strm;
+	ap_uint<128> prova = 0;
+	ap_uint<128> tmp = 0;
+	prova = prova | BASE_ADDRESS;
+//	std::cout << std::hex << prova;
+//	std::cout << std::endl;
+	prova =prova | ((tmp | IMG_SIZE) << 32);
+	tmp = 0;
+//	std::cout << prova;
+//	std::cout << std::endl;
+	prova = prova |( (tmp | KRNL_SIZE) << 64);
+	tmp = 0;
+//	std::cout << prova;
+//	std::cout << std::endl;
+	prova = prova | ((tmp | STRIDE) << 96);
+	tmp = 0;
+//	std::cout << std::endl;
+//	std::cout << prova;
+//	std::cout << std::dec << std::endl;
+	in_strm.write(prova);
+	SlidingWindowUnit(in_strm,out_mem_address,error);
 	for(int i=0; i < mat_out_dim * mat_out_dim * mat_out_dim * mat_out_dim; i++){
 		std::cout << out_mem_address.read() << std::endl;
 	}
