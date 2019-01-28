74b8e1f69ae47c3708f6b1bdbd41c19b11fd149c
Yaman Umuroglu <maltanar@gmail.com>
2019-01-28 01:07:49 +0000

    set FETCH_BLOCK_MAX to be largest power-of-two representable

--- src/main/resources/cpp/app/BitSerialMatMulAccelDriver.hpp
+++ src/main/resources/cpp/app/BitSerialMatMulAccelDriver.hpp
@@ -51,7 +51,7 @@
 #define N_STAGES                  3
 #define FETCH_ADDRALIGN           64
 #define FETCH_SIZEALIGN           64
-#define FETCH_BLOCK_MAX           (1 << BISMO_LIMIT_DRAM_BSIZE_BITS)
+#define FETCH_BLOCK_MAX           (1 << (BISMO_LIMIT_DRAM_BSIZE_BITS-1))
 
 #define max(x,y)                  (x > y ? x : y)
 #define FETCH_ALIGN               max(FETCH_ADDRALIGN, FETCH_SIZEALIGN)
