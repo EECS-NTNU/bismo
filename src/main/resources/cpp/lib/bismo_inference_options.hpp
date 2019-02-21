c5d63e389af58ace3dba8e525d6455d699f4da13
Yaman Umuroglu <maltanar@gmail.com>
2019-02-21 22:51:03 +0000

    [InferenceRT] enable instrgen by default. whee!

--- src/main/resources/cpp/lib/bismo_inference_options.hpp
+++ src/main/resources/cpp/lib/bismo_inference_options.hpp
@@ -1,10 +1,10 @@
 // enable to print debug info
 //#define DEBUG
 // use instruction generators instead
-// #define BISMORT_USE_INSTRGEN
+#define BISMORT_USE_INSTRGEN
 // enable to compare hw-produced results against sw-produced ones
 //#define BISMORT_CONV_VERIFY_AGAINST_CPU
-// #define BISMORT_MATMUL_VERIFY_AGAINST_CPU
+//#define BISMORT_MATMUL_VERIFY_AGAINST_CPU
 // #define BISMORT_P2S_VERIFY_AGAINST_CPU
 // clean p2s target buffer (set to zeros) before performing p2s
 // #define P2S_CLEAR_IN_BUF
