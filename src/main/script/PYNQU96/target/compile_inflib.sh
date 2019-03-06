71500586c76c9acfe9d22f1091bc7b6db1421cfb
Yaman Umuroglu <maltanar@gmail.com>
2019-03-06 09:46:47 +0100

    enable compiler optimizations for inflib

--- src/main/script/PYNQU96/target/compile_inflib.sh
+++ src/main/script/PYNQU96/target/compile_inflib.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -std=c++11 -I./include -I./driver -I./app -fPIC *.cpp -lcma -shared -o libbismo_inference.so
+g++ -std=c++11 -march=native -O3 -I./include -I./driver -I./app -fPIC *.cpp -lcma -shared -o libbismo_inference.so
