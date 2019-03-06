1804379e72bce4e23d9bbe546c69dd0e0a5cf5a1
Yaman Umuroglu <maltanar@gmail.com>
2019-03-06 09:26:25 +0100

    Revert "[ultra96] add neon support"
    This reverts commit ee48675203b0c12aa26b1d6bc4c1d55979437566.
    There's currently a bug with convbitserial using ARM NEON,
    once this is resolved NEON can be re-enabled.

--- src/main/script/PYNQU96/target/compile_inflib.sh
+++ src/main/script/PYNQU96/target/compile_inflib.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -std=c++11 -DGEMMBITSERIAL_USE_ARM_NEON -I./include -I./driver -I./app -fPIC *.cpp -lcma -shared -o libbismo_inference.so
+g++ -std=c++11 -I./include -I./driver -I./app -fPIC *.cpp -lcma -shared -o libbismo_inference.so
