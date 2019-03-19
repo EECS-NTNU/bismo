94601f32cc007dde757891216d102f0812565f22
Yaman Umuroglu <maltanar@gmail.com>
2019-03-19 15:03:55 +0100

    [Instrumentation] put perf details into map

--- src/main/resources/cpp/lib/bismo_inference_instrumentation.cpp
+++ src/main/resources/cpp/lib/bismo_inference_instrumentation.cpp
@@ -105,55 +105,84 @@ float InternalLayerDescriptor::getHWCompBoundWriteOI() const {
 }
 
 void InternalLayerDescriptor::printPerfSummary() {
+  instrumentationData["workload_total_binops"] = getWorkloadBinaryOpCount(true);
+  instrumentationData["workload_actual_binops"] = getWorkloadBinaryOpCount(false);
+  instrumentationData["lhs_bytes"] = lhsBytes();
+  instrumentationData["rhs_bytes"] = rhsBytes();
+  instrumentationData["res_bytes"] = resBytes();
+  instrumentationData["buf_size_bytes"] = getHWBufSize();
+  instrumentationData["peak_perf_binops"] = getHWPeakBinaryGOPS();
+  instrumentationData["fclk_mhz"] = acc->fclk_MHz();
+  instrumentationData["hw_cycles"] = getLastRuntimeCycles();
+  instrumentationData["hw_achieved_binops"] = getLastRunBinaryGOPS();
+  instrumentationData["workload_read_oi"] = getWorkloadReadOI();
+  instrumentationData["workload_write_oi"] = getWorkloadWriteOI();
+  instrumentationData["hw_peak_read_oi"] = getHWCompBoundReadOI();
+  instrumentationData["hw_peak_write_oi"] = getHWCompBoundWriteOI();
+#ifdef BISMORT_INSTRUMENTATION_VERBOSE
   std::cout << "Performance Summary ====================================" << std::endl;
-  std::cout << "Total workload: " << getWorkloadBinaryOpCount(true) << " binary ops" << std::endl;
-  std::cout << "Actual workload: " << getWorkloadBinaryOpCount(false) << " binary ops ";
-  std::cout << "(" << 100*getWorkloadBinaryOpCount(false)/getWorkloadBinaryOpCount(true) << "%)" << std::endl;
-  std::cout << "Input matrix bytes: LHS " << lhsBytes() << " RHS " << rhsBytes() << std::endl;
-  std::cout << "Result matrix bytes: " << resBytes() << std::endl;
-  std::cout << "HW input matrix buffer bytes: " << getHWBufSize() << std::endl;
-  std::cout << "HW peak perf: " << getHWPeakBinaryGOPS() << " binary GOPS" << std::endl;
-  std::cout << "HW fclk: " << acc->fclk_MHz() << " MHz" << std::endl;
-  std::cout << "Runtime: " << getLastRuntimeCycles() << " cycles, ";
+  std::cout << "Total workload: " << instrumentationData["workload_total_binops"] << " binary ops" << std::endl;
+  std::cout << "Actual workload: " << instrumentationData["workload_actual_binops"] << " binary ops ";
+  std::cout << "(" << 100*instrumentationData["workload_total_binops"]/instrumentationData["workload_actual_binops"] << "%)" << std::endl;
+  std::cout << "Input matrix bytes: LHS " << instrumentationData["lhs_bytes"] << " RHS " << instrumentationData["rhs_bytes"] << std::endl;
+  std::cout << "Result matrix bytes: " << instrumentationData["res_bytes"] << std::endl;
+  std::cout << "HW input matrix buffer bytes: " << instrumentationData["buf_size_bytes"] << std::endl;
+  std::cout << "HW peak perf: " << instrumentationData["peak_perf_binops"] << " binary GOPS" << std::endl;
+  std::cout << "HW fclk: " << instrumentationData["fclk_mhz"] << " MHz" << std::endl;
+  std::cout << "Runtime: " << instrumentationData["hw_cycles"] << " cycles, ";
   std::cout << getLastRuntimeNanoseconds() << " ns" << std::endl;
-  std::cout << "Achieved: " << getLastRunBinaryGOPS() << " binary GOPS (";
-  std::cout << 100*getLastRunBinaryGOPS() / getHWPeakBinaryGOPS() << "%)" << std::endl;
-  std::cout << "Workload OI read: " << getWorkloadReadOI();
-  std::cout << " write: " << getWorkloadWriteOI() << std::endl;
-  std::cout << "HW comp-bound OI read: " << getHWCompBoundReadOI();
-  std::cout << " write: " << getHWCompBoundWriteOI() << std::endl;
+  std::cout << "Achieved: " << instrumentationData["hw_achieved_binops"] << " binary GOPS (";
+  std::cout << 100*instrumentationData["hw_achieved_binops"] / instrumentationData["peak_perf_binops"] << "%)" << std::endl;
+  std::cout << "Workload OI read: " << instrumentationData["workload_read_oi"];
+  std::cout << " write: " << instrumentationData["workload_write_oi"] << std::endl;
+  std::cout << "HW comp-bound OI read: " << instrumentationData["hw_peak_read_oi"];
+  std::cout << " write: " << instrumentationData["hw_peak_write_oi"] << std::endl;
   std::cout << "========================================================" << std::endl;
+#endif
 }
 
 void InternalLayerDescriptor::printPerfDetails() {
-  int colwidth = 11;
-  acc->printStateBreakdown();
-
-  std::cout << "Memory System ==========================================" << std::endl;
   size_t rd_total = (getNumBytesToFetch());
   float rd_bw = (float) rd_total / getLastRuntimeCycles();
   float rd_fetchact_bw = (float) getNumBytesToFetch() / acc->getStateBreakdown(stgFetch, csRun);
-  std::cout << "Total DRAM reads: " << rd_total << " bytes" << std::endl;
-  std::cout << "Matrix DRAM reads: " << getNumBytesToFetch() << " bytes (";
-  std::cout << 100*((float)(getNumBytesToFetch())/rd_total) << "% of total)" << std::endl;
-  std::cout << "HW theoretical peak read bandwidth: " << getHWReadBW() << " bytes/cycle" << std::endl;
+  float wr_bw = (float)getNumBytesToWrite() / getLastRuntimeCycles();
+  float wr_resact_bw = (float) getNumBytesToWrite() / acc->getStateBreakdown(stgResult, csRun);
+  float exec_eff = getWorkloadBinaryOpCount(true) / ((acc->getStateBreakdown(stgExec, csRun) * getHWPeakBinaryOpsPerCycle()));
+  instrumentationData["dram_read_bytes"] = rd_total;
+  instrumentationData["hw_peak_read_bw"] = getHWReadBW();
+  instrumentationData["dram_write_bytes"] = getNumBytesToWrite();
+  instrumentationData["hw_peak_write_bw"] = getHWWriteBW();
+  instrumentationData["fetch_idle"] = acc->getStateBreakdown(stgFetch, csGetCmd);
+  instrumentationData["exec_idle"] = acc->getStateBreakdown(stgExec, csGetCmd);
+  instrumentationData["result_idle"] = acc->getStateBreakdown(stgResult, csGetCmd);
+  instrumentationData["fetch_run"] = acc->getStateBreakdown(stgFetch, csRun);
+  instrumentationData["exec_run"] = acc->getStateBreakdown(stgExec, csRun);
+  instrumentationData["result_run"] = acc->getStateBreakdown(stgResult, csRun);
+  instrumentationData["fetch_snd"] = acc->getStateBreakdown(stgFetch, csSend);
+  instrumentationData["exec_snd"] = acc->getStateBreakdown(stgExec, csSend);
+  instrumentationData["result_snd"] = acc->getStateBreakdown(stgResult, csSend);
+  instrumentationData["fetch_rcv"] = acc->getStateBreakdown(stgFetch, csReceive);
+  instrumentationData["exec_rcv"] = acc->getStateBreakdown(stgExec, csReceive);
+  instrumentationData["result_rcv"] = acc->getStateBreakdown(stgResult, csReceive);
+#ifdef BISMORT_INSTRUMENTATION_VERBOSE
+  int colwidth = 11;
+  acc->printStateBreakdown();
+  std::cout << "Memory System ==========================================" << std::endl;
+  std::cout << "Total DRAM reads: " << instrumentationData["dram_read_bytes"] << " bytes" << std::endl;
+  std::cout << "HW theoretical peak read bandwidth: " << instrumentationData["hw_peak_read_bw"] << " bytes/cycle" << std::endl;
   std::cout << "Average DRAM read bandwidth: " << rd_bw << " bytes/cycle (";
   std::cout << 100*rd_bw/getHWReadBW() << "%)" << std::endl;
   std::cout << "Fetch efficiency: " << rd_fetchact_bw << " bytes/cycle (";
   std::cout << 100*rd_fetchact_bw/getHWReadBW() << "%)" << std::endl;
-
-  std::cout << "DRAM writes: " << getNumBytesToWrite() << " bytes" << std::endl;
-  float wr_bw = (float)getNumBytesToWrite() / getLastRuntimeCycles();
-  float wr_resact_bw = (float) getNumBytesToWrite() / acc->getStateBreakdown(stgResult, csRun);
-  std::cout << "HW theoretical peak wr bandwidth: " << getHWWriteBW() << " bytes/cycle" << std::endl;
+  std::cout << "DRAM writes: " << instrumentationData["dram_write_bytes"] << " bytes" << std::endl;
+  std::cout << "HW theoretical peak wr bandwidth: " << instrumentationData["hw_peak_write_bw"] << " bytes/cycle" << std::endl;
   std::cout << "Effective wr bandwidth: " << wr_bw << " bytes/cycle (";
   std::cout << 100*wr_bw/getHWWriteBW() << "%)" << std::endl;
   std::cout << "Result wr bandwidth: " << wr_resact_bw << " bytes/cycle (";
   std::cout << 100*wr_resact_bw/getHWWriteBW() << "%)" << std::endl;
-
-  float exec_eff = getWorkloadBinaryOpCount(true) / ((acc->getStateBreakdown(stgExec, csRun) * getHWPeakBinaryOpsPerCycle()));
   std::cout << "Execute stage efficiency: " << 100*exec_eff << "%" << std::endl;
   std::cout << "========================================================" << std::endl;
+#endif
 }
 #endif
 }
