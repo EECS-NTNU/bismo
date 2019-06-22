a788ce19ab56d998608718441c2403196792311a
Yaman Umuroglu <maltanar@gmail.com>
2019-06-22 19:40:39 +0100

    [ZCU104] fix tcl clock setting

--- src/main/script/PYNQZCU104/host/make-vivado-project.tcl
+++ src/main/script/PYNQZCU104/host/make-vivado-project.tcl
@@ -76,10 +76,8 @@ set_property -dict [list CONFIG.PSU__SAXIGP2__DATA_WIDTH {128}] [get_bd_cells zy
 #set_property -dict [list CONFIG.PSU__SAXIGP2__DATA_WIDTH {128} CONFIG.PSU__SAXIGP3__DATA_WIDTH {128} CONFIG.PSU__USE__S_AXI_GP4 {1} CONFIG.PSU__SAXIGP4__DATA_WIDTH {128} CONFIG.PSU__SAXIGP5__DATA_WIDTH {128}] [get_bd_cells zynq_ultra_ps_e_0]
 # TODO set frequency here
 # set_property -dict [list CONFIG.PCW_FPGA0_PERIPHERAL_FREQMHZ $config_freq CONFIG.PCW_FPGA1_PERIPHERAL_FREQMHZ {142.86} CONFIG.PCW_FPGA2_PERIPHERAL_FREQMHZ {200} CONFIG.PCW_FPGA3_PERIPHERAL_FREQMHZ {166.67} CONFIG.PCW_EN_CLK1_PORT {1} CONFIG.PCW_EN_CLK2_PORT {1} CONFIG.PCW_EN_CLK3_PORT {1} CONFIG.PCW_USE_M_AXI_GP0 {1}] $ps7
-#set clkdiv [expr { int(1500/$config_freq) }]
-#set_property -dict [list CONFIG.PSU__CRL_APB__PL0_REF_CTRL__DIVISOR0 $clkdiv] [get_bd_cells zynq_ultra_ps_e_0]
-set_property -dict [list CONFIG.PSU__FPGA_PL0_ENABLE {1} CONFIG.PSU__CRL_APB__PL0_REF_CTRL__FREQMHZ {$config_freq}] [get_bd_cells zynq_ultra_ps_e_0]
-
+set clkdiv [expr { int(1200/$config_freq) }]
+set_property -dict [list CONFIG.PSU__FPGA_PL0_ENABLE {1} CONFIG.PSU__CRL_APB__PL0_REF_CTRL__SRCSEL {RPLL} CONFIG.PSU__OVERRIDE__BASIC_CLOCK {1} CONFIG.PSU__CRL_APB__PL0_REF_CTRL__DIVISOR0 $clkdiv] [get_bd_cells zynq_ultra_ps_e_0]
 
 # add the accelerator RTL module into the block design
 create_bd_cell -type module -reference PYNQZCU104Wrapper PYNQZCU104Wrapper_0
