b388d7825731c2fda4ca60ac0bc0c4d5ec768a58
Magnus Sjalander <magnus.sjalander@ntnu.no>
2019-12-25 15:30:49 +0100

    Suppressed int-to-pointer-cast warnings

--- src/main/script/VerilatedTester/target/compile_rtlib.sh
+++ src/main/script/VerilatedTester/target/compile_rtlib.sh
@@ -31,4 +31,4 @@
 #!/bin/bash
 VERILATOR_SRC_DIR="/usr/share/verilator/include"
 
-g++ -std=c++11 -march=native -O0 -I$VERILATOR_SRC_DIR -Iverilog/verilated -I./hls_include -I./driver -I./test -fPIC rtlib/*.cpp driver/*.cpp verilog/verilated/*.cpp -shared -o libbismo_rt.so
+g++ -std=c++11 -march=native -O0 -Wno-int-to-pointer-cast -I$VERILATOR_SRC_DIR -Iverilog/verilated -I./hls_include -I./driver -I./test -fPIC rtlib/*.cpp driver/*.cpp verilog/verilated/*.cpp -shared -o libbismo_rt.so
